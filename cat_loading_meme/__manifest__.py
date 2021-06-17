706db3f52d2bf7a9490e08d9fac84107bff56bdc
Huy <ngochuy97hp@gmail.com>
2021-06-17 16:53:12 +0700

    Init cat meme :)

---
+++ cat_loading_meme/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': "Cat Meme Loading Spinner",
+
+    'summary': """
+        """,
+
+    'author': "Trouble Maker",
+
+    'category': 'Meme',
+
+    'version': '1.0',
+
+    'depends': ['web'],
+
+    'data': [
+        'views/assets.xml',
+    ],
+    'qweb': [
+        'static/src/xml/base.xml',
+    ],
+
+    'images': [
+        # 'static/description/main_screenshot.png'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ cat_loading_meme/static/src/js/loading.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    $.blockUI.defaults.overlayCSS = {};
+    $.blockUI.defaults.overlayCSS['backdrop-filter'] = 'blur(10px) grayscale(0.5)';
+});
